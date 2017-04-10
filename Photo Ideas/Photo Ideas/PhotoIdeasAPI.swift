ea619cd837c176caef2d0eba741a34a2af6e5ca5
BenEmdon <benjaminemdon@gmail.com>
2017-04-10 17:55:25 -0400

    added dependencies added Moya TargetType for backend service added image loader made programatically added assests

---
+++ Photo Ideas/Photo Ideas/PhotoIdeasAPI.swift
@@ -0,0 +1,64 @@
+//
+//  PhotoIdeasAPI.swift
+//  Photo Ideas
+//
+//  Created by Benjamin Emdon on 2017-04-10.
+//  Copyright © 2017 Benjamin Emdon. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum PhotoIdeasAPI {
+	case image(data: Data)
+	case subjects(id: String?)
+}
+
+extension PhotoIdeasAPI: TargetType {
+	var baseURL: URL { return URL(string: "https://photo-ideas-backend.herokuapp.com")! }
+
+	var path: String {
+		switch self {
+		case .image(_):
+			return "/image"
+		case .subjects(let id):
+			if let id = id {
+				return "/subjects/\(id)"
+			} else {
+				return "/subjects"
+			}
+		}
+	}
+
+	var method: Moya.Method {
+		switch self {
+		case .image(_):
+			return .post
+		case .subjects(_):
+			return .get
+		}
+	}
+
+	var parameters: [String : Any]? {
+		switch self {
+		case .image(let data):
+			return [
+				"image": data.base64EncodedString()
+			]
+		case .subjects(_):
+			return nil
+		}
+	}
+
+	var parameterEncoding: ParameterEncoding {
+		return URLEncoding.default
+	}
+
+	var task: Task {
+		return .request
+	}
+
+	var sampleData: Data {
+		return "".data(using: .utf8)! // TODO implement fixtures for tests
+	}
+}
